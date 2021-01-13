5e35d717f64bcae23b22a1603039bfbbf66b7394
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-01-13 00:36:49 -0600

    Starting Javascript shell

---
+++ Assets/script.js
@@ -0,0 +1,35 @@
+var startButton = document.querySelector(".startButton");
+
+// Make box for question
+var question = document.createElement('div')
+ 
+// Make questions:
+function question1 (choice1, choice2, choice3, choice4) {
+// Create div element to hold each question
+    var question = document.createElement("div");
+    var node = document.createTextNode("What are the four main types of data?");
+    question.appendChild(node);
+
+    // var emptyString = "";
+ 
+}; 
+
+function question2 (choice1, choice2, choice3, choice4) {
+    var question = document.createElement('div');
+};
+
+function question3 (choice1, choice2, choice3, choice4) {
+    var question = document.createElement('div');
+};
+
+function question4 (choice1, choice2, choice3, choice4) {
+    var question = document.createElement('div');
+    let question = 
+};
+
+
+startButton.addEventListener("click", function () {
+// Upon clicking the start button, needs to show 5 divs (question, choice1, choice2, choice3, choice4)
+    return question1();
+
+});
